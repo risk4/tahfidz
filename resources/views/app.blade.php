d97324e65b3932cd4cfc4f1306454d764ae59bdf
Azzam Azmi <riskakreasikomputer@gmail.com>
2026-08-22 22:23:43 +0700

    fix: render favicon langsung dari server di app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
+@php
+    $appSetting = \Illuminate\Support\Facades\Schema::hasTable('settings')
+        ? \App\Domain\Settings\Models\AppSetting::whereIn('key', ['application.favicon_path', 'application.logo_path', 'profile.app_name'])->pluck('value', 'key')
+        : collect();
+    $faviconPath = $appSetting->get('application.favicon_path');
+    $appName = $appSetting->get('profile.app_name') ?? config('app.name', 'Tahfidz App');
+    $faviconUrl = $faviconPath ? asset('storage/' . $faviconPath) : null;
+@endphp
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    @if ($faviconUrl)
+        <link rel="icon" href="{{ $faviconUrl }}" />
+    @else
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    @endif
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Tahfidz App</title>
+    <title>{{ $appName }}</title>
     @fonts
     @vite(['resources/css/app.css', 'resources/js/app.ts'])
 </head>
